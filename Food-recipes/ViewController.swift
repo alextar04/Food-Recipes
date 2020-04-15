578c86bf2b5e52e7335cdfa7d418bdcbcdd3ed60
alextar04 <taranlesha@mail.ru>
2020-04-15 21:21:41 +0300

    Initial Commit

---
+++ Food-recipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food-recipes
+//
+//  Created by Alexey Taran on 15.04.2020.
+//  Copyright © 2020 Alexey Taran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
